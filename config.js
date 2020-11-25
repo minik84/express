5baa68b93079275de38fcb14851b704cc0c77952
Dominik Ostrowski <ostrowski.dominik@gmail.com>
2020-11-25 16:42:19 +0100

    admin login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  keySession: ['TWOJKLUCZ'],
+  // maxAgeSession: 24 * 60 * 60 * 1000,
+  maxAgeSession: 5 * 1000,
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res, next) => {
+  if (!req.session.admin) {
+    res.redirect('/login')
+
+    return;
+  }
+
+  next();
+})
+
 /* GET home page. */
 router.get('/', (req, res) => {
+
+  // if (!req.session.admin) {
+  //   res.redirect('/login')
+
+  //   return;
+  // }
+
   res.render('admin', { title: 'Admin' });
 });
 
