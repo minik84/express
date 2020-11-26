0d20373eb98047afbfd2576e3945384380fd3cc5
Dominik Ostrowski <ostrowski.dominik@gmail.com>
2020-11-26 17:27:30 +0100

    admin loggin, admin news

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
+  db: '',
   keySession: ['TWOJKLUCZ'],
   // maxAgeSession: 24 * 60 * 60 * 1000,
-  maxAgeSession: 5 * 1000,
+  maxAgeSession: 5 * 60 * 1000,
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const News = require('../models/news');
+
 const router = express.Router();
 
 router.all('*', (req, res, next) => {
@@ -13,14 +15,35 @@ router.all('*', (req, res, next) => {
 
 /* GET home page. */
 router.get('/', (req, res) => {
-
-  // if (!req.session.admin) {
-  //   res.redirect('/login')
-
-  //   return;
-  // }
-
-  res.render('admin', { title: 'Admin' });
+  News.find({}, (err, data) => {
+    res.render('admin/index.pug', { title: 'Admin', data });
+  })
 });
+
+router.get('/news/add', (req, res) => {
+  res.render('admin/news-form.pug', { title: 'Dodaj artykuł', body: {}, errors: {} });
+})
+
+router.post('/news/add', (req, res) => {
+  const body = req.body;
+
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+
+  newsData.save(err => {
+    if (err) {
+      res.render('admin/news-form.pug', { title: 'Dodaj artykuł', body, errors });
+      return;
+    } else {
+      res.redirect('/admin')
+    }
+  })
+})
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, err => {
+    res.redirect('/admin')
+  })
+})
 
 module.exports = router;
