6e782edb6f11174a974fef2c948a8c43a474a063
Dominik Ostrowski <ostrowski.dominik@gmail.com>
2020-11-27 12:37:19 +0100

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: '',
+  db: 'mongodb+srv://admin:uXFLLEvK6Zh5goVv@cluster0.ahlf3.mongodb.net/<dbname>?retryWrites=true&w=majority',
   keySession: ['TWOJKLUCZ'],
   // maxAgeSession: 24 * 60 * 60 * 1000,
   maxAgeSession: 5 * 60 * 1000,
